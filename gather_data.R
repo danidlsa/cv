3bde6c2b44280e8a4a36cf2a4f7617a4e30915d2
danidlsa <danidlsa@hotmail.com>
2023-09-22 20:38:30 -0300

    CV code + pdf

---
+++ gather_data.R
@@ -0,0 +1,4 @@
+position_data <- read.csv("csvs/positions.csv", fileEncoding = "Windows-1252")
+publications <- read.csv("csvs/publicaciones_2.csv", fileEncoding = "Windows-1252", sep="\t")
+projects <- read.csv2("csvs/recent projects.csv", fileEncoding = "Windows-1252")
+dissemination <- read.csv2("csvs/dissemination.csv", fileEncoding = "Windows-1252", sep="\t")
